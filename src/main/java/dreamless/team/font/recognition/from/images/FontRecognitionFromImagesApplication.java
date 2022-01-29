67fe6c3aab3276b992d9046bbe42de26a0835ff7
ValentynNagay <nagayvalentyn@gmail.com>
2022-01-29 11:59:25 +0200

    Update root package name

--- src/main/java/dreamless/team/font/recognition/from/images/FontRecognitionFromImagesApplication.java
+++ src/main/java/dreamless/team/font/recognition/from/images/FontRecognitionFromImagesApplication.java
@@ -1,4 +1,4 @@
-package dreamless.team.fontrecognishionfromimages;
+package dreamless.team.font.recognition.from.images;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
