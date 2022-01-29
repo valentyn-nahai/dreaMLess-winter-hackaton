96e98b0df052a31f3dd43753ebb0c5eeb0c513a0
ValentynNagay <nagayvalentyn@gmail.com>
2022-01-29 11:54:46 +0200

    Create basic Spring app

---
+++ src/main/java/dreamless/team/fontrecognishionfromimages/FontRecognitionFromImagesApplication.java
@@ -0,0 +1,13 @@
+package dreamless.team.fontrecognishionfromimages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FontRecognitionFromImagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FontRecognitionFromImagesApplication.class, args);
+	}
+
+}
